6ca2117d2326bdd585e60c0da6c0165cce671b5d
huMonster <monster.hu@outlook.com>
2021-04-25 16:30:18 +0800

    添加动态规划解法

--- Fib.java
+++ Fib.java
@@ -7,20 +7,25 @@ package com.algorithm.leetcode;
  * @Version 1.0
  */
 public class Fib {
+
+    // 题目限制条件为[0, 30]，共31个数
+    private static int[] record = new int[31];
     /**
      * 递归： F(n) = F(n - 1) + F(n - 2)
+     * 问题：大量重复计算
+     * 优化：先判断当前位置是否计算过，如果没有，则先计算在存储，如果有，直接返回计算过的值
      *
      * @param n F(n)
      * @return
      */
     public static int recursion(int n){
-        if(n == 0){
-            return 0;
+        if(n <= 1){
+            return n;
         }
-        if(n <= 2){
-            return 1;
+        if(record[n] == 0){
+            record[n] = recursion(n - 1) + recursion(n - 2);
         }
-        return recursion(n - 1) + recursion(n - 2);
+        return record[n];
     }
 
     /**
@@ -44,9 +49,25 @@ public class Fib {
         }
         return result;
     }
-    
+
+    /**
+     * 动态规划
+     * @param n
+     */
+    public static int dp(int n){
+        if(n <= 1){
+            return n;
+        }
+        int[] result = new int[n + 1];
+        result[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(recursion(9));
-        System.out.println(nonRecursion(9));
+        int n = 9;
+        System.out.println(recursion(n));
     }
 }
